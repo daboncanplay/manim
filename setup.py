314a4206d89df177a7186950de6061cad7239da9
daboncanplay <daboncanplay@yahoo.com>
2025-02-13 13:38:55 -0700

    bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
     
     setup(
-        name='dave-manim',
+        name='manim_dave',
         version='0.1.0',
         packages=find_packages(),
         install_requires=[], # Add any dependencies here
